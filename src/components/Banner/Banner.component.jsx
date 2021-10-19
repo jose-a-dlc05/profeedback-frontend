0c326dad048dea8a249baec2084b775ffe09c5f6
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-19 01:24:31 -0400

    Switched all banner code to its own component with styling

---
+++ src/components/Banner/Banner.component.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import image from '../../assets/image_one.png';
+import '../Banner/Banner.styles.scss';
+
+function Banner() {
+	return (
+		<div className='banner'>
+			<div className='banner-icon'>
+				<a href='https://localhost:3000'>
+					<img src={image} alt='product feedback logo' />
+				</a>
+				<span className='suggestions__amount'>0 Suggestions</span>
+				<div className='dropdown'>
+					<span className='dropdown-type'>Sort by:</span>
+					<button
+						className='btn dropdown-toggle'
+						type='button'
+						id='dropdownMenuButton1'
+						data-bs-toggle='dropdown'
+						aria-expanded='false'
+					>
+						Most Upvotes
+					</button>
+					<ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+						<li>
+							<a className='dropdown-item' href='http://localhost:3000/'>
+								Most Upvotes
+								<span className='checkmark-active'>
+									<i class='fas fa-check'></i>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a className='dropdown-item' href='http://localhost:3000/'>
+								Least Upvotes
+								<span className='checkmark-not-active'>
+									<i class='fas fa-check'></i>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a className='dropdown-item' href='http://localhost:3000/'>
+								Most Comments
+								<span className='checkmark-not-active'>
+									<i class='fas fa-check'></i>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a className='dropdown-item' href='http://localhost:3000/'>
+								Least Comments
+								<span className='checkmark-not-active'>
+									<i class='fas fa-check'></i>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className='add-btn'>
+				<button type='button' className='btn btn-primary'>
+					+ Add Feedback
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Banner;
--- src/pages/Home/Home.component.jsx
+++ src/pages/Home/Home.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tile from '../../components/Tile/Tile.component';
-import image from '../../assets/image_one.png';
+import Banner from '../../components/Banner/Banner.component';
 import './Home.styles.scss';
 
 function Home() {
@@ -15,70 +15,8 @@ function Home() {
 				</Tile>
 			</div>
 			<div className='col-8 feedback-area'>
-				<div className='banner'>
-					<div className='banner-icon'>
-						<a href='https://localhost:3000'>
-							<img src={image} alt='product feedback logo' />
-						</a>
-						<span className='suggestions__amount'>6 Suggestions</span>
-						<div className='dropdown'>
-							<span className='dropdown-type'>Sort by:</span>
-							<button
-								className='btn dropdown-toggle'
-								type='button'
-								id='dropdownMenuButton1'
-								data-bs-toggle='dropdown'
-								aria-expanded='false'
-							>
-								Most Upvotes
-							</button>
-							<ul
-								className='dropdown-menu'
-								aria-labelledby='dropdownMenuButton1'
-							>
-								<li>
-									<a className='dropdown-item' href='http://localhost:3000/'>
-										Most Upvotes
-										<span className='checkmark-active'>
-											<i class='fas fa-check'></i>
-										</span>
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='http://localhost:3000/'>
-										Least Upvotes
-										<span className='checkmark-not-active'>
-											<i class='fas fa-check'></i>
-										</span>
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='http://localhost:3000/'>
-										Most Comments
-										<span className='checkmark-not-active'>
-											<i class='fas fa-check'></i>
-										</span>
-									</a>
-								</li>
-								<li>
-									<a className='dropdown-item' href='http://localhost:3000/'>
-										Least Comments
-										<span className='checkmark-not-active'>
-											<i class='fas fa-check'></i>
-										</span>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-
-					<div className='add-btn'>
-						<button type='button' className='btn btn-primary'>
-							+ Add Feedback
-						</button>
-					</div>
-				</div>
-				<div>Cards</div>
+				<Banner />
+				<div className='feedback-cards'>Cards</div>
 			</div>
 		</div>
 	);
