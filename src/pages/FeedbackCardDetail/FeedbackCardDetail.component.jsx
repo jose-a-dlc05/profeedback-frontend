f8c3c093669913afc574f9836257b5de994f5dbd
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-22 23:40:02 -0400

    Added the feedback card to the details page

--- src/pages/FeedbackCardDetail/FeedbackCardDetail.component.jsx
+++ src/pages/FeedbackCardDetail/FeedbackCardDetail.component.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './FeedbackCardDetail.styles.scss';
 
 function FeedbackCardDetail({ match }) {
 	const [singleFeedback, setSingleFeedback] = useState(null);
@@ -28,14 +30,48 @@ function FeedbackCardDetail({ match }) {
 		getComments();
 	}, []);
 
+	console.log(singleFeedback);
+	console.log(comments);
+
 	if (singleFeedback === null) {
 		return <div></div>;
 	}
 
 	return (
-		<div>
-			{JSON.stringify(singleFeedback)}
-			{JSON.stringify(comments)}
+		<div className='feedback-card-detail'>
+			<div className='feedback-detail'>
+				<div className='feedback-control'>
+					<div className='link-wrapper'>
+						<Link to='/' className='go-back-link'>
+							<span>
+								<i className='fas fa-angle-left'></i>
+							</span>
+							Go Back
+						</Link>
+					</div>
+					<button type='button' className='btn btn-edit'>
+						Edit Feedback
+					</button>
+				</div>
+				<div className='feedback-card' style={{ marginTop: '25px' }}>
+					<div className='upvote-btn'>
+						{/* <span className='arrow-up'>^</span>
+					<span className='upvote-number'>{data.upvotes}</span> */}
+					</div>
+					<div className='card-overview'>
+						<h4>{singleFeedback[0].title}</h4>
+						<p>{singleFeedback[0].description}</p>
+						<span>{singleFeedback[0].category}</span>
+					</div>
+					<div className='comment'>
+						<span>
+							<i className='fas fa-comment'></i>
+						</span>
+						<span>{Number(comments.length)}</span>
+					</div>
+				</div>
+			</div>
+			<div className='comment-card'>Comments</div>
 		</div>
 	);
 }
