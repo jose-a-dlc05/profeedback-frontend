a481b59ea36eee08aafaa1b60c359f39cd65176e
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-27 20:41:30 -0400

    Added image avatar next to comment name

--- src/components/Comment/Comment.component.jsx
+++ src/components/Comment/Comment.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { uniqueNamesGenerator, names } from 'unique-names-generator';
 import '../Comment/Comment.styles.scss';
+import image from '../../assets/img_avatar.png';
 
 function Comment({ detail }) {
 	const config = {
@@ -10,7 +11,16 @@ function Comment({ detail }) {
 	const characterName = uniqueNamesGenerator(config);
 	return (
 		<div className='comment-card'>
-			<p className='user'>{characterName}</p>
+			<p className='user'>
+				<img
+					src={image}
+					alt='Avatar'
+					width='30px'
+					height='30px'
+					style={{ marginRight: '5px', borderRadius: '50%' }}
+				/>
+				{characterName}
+			</p>
 			<p className='comment-post'>{detail.content}</p>
 		</div>
 	);
