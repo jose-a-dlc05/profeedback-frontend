9a78ada845d1c69d4af5300ba7a26c10b1e0beb6
Jose De La Cruz <jose.delcruz0490@icloud.com>
2024-10-28 06:20:12 -0400

    Added the upvote button

--- src/components/FeedbackCard/FeedbackCard.component.tsx
+++ src/components/FeedbackCard/FeedbackCard.component.tsx
@@ -4,21 +4,29 @@ import './FeedbackCard.styles.scss';
 
 function FeedbackCard({ data }) {
 	return (
-		<div className='feedback-card'>
-			<div className='card-overview'>
-				<Link to={`/feedback-product/${data.id}`}>
-					<h4>{data.title}</h4>{' '}
-				</Link>
-				<p>{data.description}</p>
-				<span>{data.category}</span>
-			</div>
-			<div className='comment'>
-				<span>
-					<i className='fas fa-comment'></i>
+		<>
+			<div className='feedback-card'>
+				<span className='badge upvote-btn'>
+					<span>
+						<i className='fas fa-angle-up'></i>
+					</span>
+					{data.upvotes}
 				</span>
-				<span>{Number(data.comments)}</span>
+				<div className='card-overview'>
+					<Link to={`/feedback-product/${data.id}`}>
+						<h4>{data.title}</h4>{' '}
+					</Link>
+					<p>{data.description}</p>
+					<span>{data.category}</span>
+				</div>
+				<div className='comment'>
+					<span>
+						<i className='fas fa-comment'></i>
+					</span>
+					<span>{Number(data.comments)}</span>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
